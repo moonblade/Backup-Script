353cc983c1787db5bd60d64213e1c0b1ef4ecc50
moonblade <moonblade168@gmail.com>
2020-12-28 08:13:19 +0530

    fullpath

--- backup.sh
+++ backup.sh
@@ -1,2 +1,2 @@
 /Users/moonblade/workspace/backupScripts/prepareBackup.sh
-rclone sync /Users/moonblade/workspace --filter-from /Users/moonblade/workspace/backupScripts/filter.conf box:mac_backup
+/usr/local/bin/rclone sync /Users/moonblade/workspace --filter-from /Users/moonblade/workspace/backupScripts/filter.conf box:mac_backup
